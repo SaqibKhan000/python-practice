ef263e8f96e8417fe9c79607abbc1a2477465022
M Saqib Khan <saqibkhannn543@gmail.com>
2026-06-07 21:30:57 +0500

    Python basics

---
+++ practice.py
@@ -0,0 +1,25 @@
+
+
+### Data Type ###
+
+# 1) Numeric DataTypes
+# a) Integer b) Float c) 
+
+# 2) Sequence DataTypes
+# a) String b) List c) Tuple
+
+# 3) Boolean DataType
+# a) True b) False
+
+# 4) Set DataType
+  
+# 5) Dictionary DataType
+
+# 6) None DataType
+
+
+# input() function is used to take input from user
+
+
+# Conditional Statements
+
