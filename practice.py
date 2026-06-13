95aed32fdbf0183051421994a9da8530d9def85b
M Saqib Khan <saqibkhannn543@gmail.com>
2026-06-13 22:27:01 +0500

    Small task about generator function

--- practice.py
+++ practice.py
@@ -32,5 +32,31 @@
 
 
 
+## Decorator: Esa function jo ksi dosre function k kaam ko enhance ya modify kre bina os function
+# k code ko change kye
+# def logger(login):
+#     def wrapper():
+#         print("Start")
+#         login()
+#         print("End")
+#     return wrapper
+
+# def login():
+#     print("User logged in")
+# logger(login())
 
 
+
+## Generator: Ek esa function jo values ko sequene mein generate kre. Aur yeild keyword k through
+# hm es function se at a time ek he value return kr skte hai. 
+
+# import time
+# def counter(max_num):
+#     num = 1
+#     while num <= max_num:
+#         yield num
+#         num += 1
+
+# for number in counter(10):
+#    print(number)
+#    time.sleep(1)
